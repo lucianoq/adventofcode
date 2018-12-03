23ee15e65bbcad979203f6f85f16403d5f9ce9ae
sonictrain <nicola.brucoli92@gmail.com>
2018-12-03 01:59:42 +0100

    useless variables removed

--- 2018/2/part1/sonictrain/main.py
+++ 2018/2/part1/sonictrain/main.py
@@ -4,8 +4,6 @@ string = list()
 alpha = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
 twice = 0
 triple = 0
-check2 = 0
-check3 = 0
 
 for line in sys.stdin :
     string.append(line)
