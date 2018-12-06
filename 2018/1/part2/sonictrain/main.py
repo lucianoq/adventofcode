168bb142ca4648a1eba11ca0c9c45b77bfb6fb9a
sonictrain <nicola.brucoli92@gmail.com>
2018-12-06 01:00:26 +0100

    2018.1/2 not working yet

--- 2018/1/part2/sonictrain/main.py
+++ 2018/1/part2/sonictrain/main.py
@@ -1,9 +1,16 @@
 import sys
-
+hz_list = list()
 hz = 0
+i = 0
+twice = True
 
-for line in sys.stdin :
-    hz += int(line)
-
-    for line in sys.stdout :
-        print (line)
+while twice == True:
+    for line in sys.stdin :
+        hz += int(line)
+        hz_list.append(hz)
+        if i > 0 :
+            if hz_list[i] == hz_list[i-1] :
+                print (hz_list[i])
+                twice = False
+        i+=1
+                
