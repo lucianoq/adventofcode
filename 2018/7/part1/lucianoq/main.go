7934d113d845fd5dd0a3a61ac8a8b26c2bbdb125
Luciano Quercia <luciano.quercia@numencapital.com>
2018-12-10 11:28:07 +0000

    Solution for 2018.7/1

--- 2018/7/part1/lucianoq/main.go
+++ 2018/7/part1/lucianoq/main.go
@@ -3,29 +3,78 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"sort"
 	"strings"
-
- 	"github.com/davecgh/go-spew/spew"
 )
 
+var graph map[string][]string
+var order string
+
 func main() {
-	graph := make(map[string][]string)
+	graph = make(map[string][]string)
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
-		var x, y string
-		fmt.Sscanf(line, "Step %s must be finished before step %s can begin.", &x, &y)
+		if line!="" {
+			var x, y string
+			n, err := fmt.Sscanf(line, "Step %s must be finished before step %s can begin.", &x, &y)
+			if err != nil || n != 2 {
+				log.Fatal(err)
+			}
 
-		if graph[x] == nil {
-			graph[x] = make([]string, 0)
+			if graph[x] == nil {
+				graph[x] = make([]string, 0)
+			}
+			if graph[y] == nil {
+				graph[y] = make([]string, 0)
+			}
+
+			graph[y] = append(graph[y], x)
 		}
-		if graph[y] == nil {
-			graph[y] = make([]string, 0)
-		}
-		graph[y] = append(graph[y], x)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
 	}
 
-	spew.Dump(graph)
+	for next := findNext(); next != ""; next = findNext() {
+		solve(next)
+	}
+
+	fmt.Println(order)
+
+}
+
+func findNext() string {
+	next := make([]string, 0)
+	for k, c := range graph {
+		if len(c) == 0 {
+			next = append(next, k)
+		}
+	}
+	if len(next) == 0 {
+		return ""
+	}
+	sort.Strings(next)
+	return next[0]
+}
+
+func solve(n string) {
+	order += n
+
+	delete(graph, n)
+	for k, c := range graph {
+		del := -1
+		for i := 0; i < len(c); i++ {
+			if c[i] == n {
+				del = i
+				break
+			}
+		}
+		if del != -1 {
+			graph[k] = append(graph[k][:del], graph[k][del+1:]...)
+		}
+	}
 }
