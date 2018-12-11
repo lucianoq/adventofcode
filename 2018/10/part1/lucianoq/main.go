d86a47043e3fdba74646c09287ff02bedb566784
Luciano Quercia <luciano.quercia@numencapital.com>
2018-12-11 15:06:21 +0000

    assignment and solution for 2018.10/1

---
+++ 2018/10/part1/lucianoq/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strings"
+)
+
+type Star struct {
+	X    int
+	Y    int
+	VelX int
+	VelY int
+}
+
+func (s *Star) Current(time int) (x, y int) {
+	return s.X + time*s.VelX, s.Y + time*s.VelY
+}
+
+var stars []*Star
+
+func main() {
+	stars = make([]*Star, 0)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line != "" {
+			line = strings.Replace(line, " ", "", -1)
+			var x, y, velX, velY int
+			n, err := fmt.Sscanf(line, "position=<%d,%d>velocity=<%d,%d>", &x, &y, &velX, &velY)
+			if err != nil || n != 4 {
+				log.Fatal(err)
+			}
+			stars = append(stars, &Star{x, y, velX, velY})
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; ; i++ {
+		Sky(i)
+		//time.Sleep(200 * time.Millisecond)
+	}
+
+}
+
+func Sky(time int) {
+	interpolatedStars := make([]*Star, 0)
+	minX, minY := math.MaxInt64, math.MaxInt64
+	maxX, maxY := math.MinInt64, math.MinInt64
+	for _, s := range stars {
+		x, y := s.Current(time)
+		interpolatedStars = append(interpolatedStars, &Star{x, y, 0, 0})
+		if x < minX {
+			minX = x
+		}
+		if x > maxX {
+			maxX = x
+		}
+		if y < minY {
+			minY = y
+		}
+		if y > maxY {
+			maxY = y
+		}
+	}
+
+	// Normalize
+	for _, s := range interpolatedStars {
+		s.X -= minX
+		s.Y -= minY
+	}
+	maxX -= minX
+	maxY -= minY
+	maxX ++
+	maxY ++
+
+	if maxX < 64 {
+		// Initialize
+		sky := make([][]bool, maxX+1)
+		for i := 0; i < maxX+1; i++ {
+			sky[i] = make([]bool, maxY+1)
+		}
+
+		// Populate
+		for _, s := range interpolatedStars {
+			sky[s.X][s.Y] = true
+		}
+
+		for j := 0; j < maxY; j++ {
+			for i := 0; i < maxX; i++ {
+				if sky[i][j] {
+					fmt.Print("#")
+				} else {
+					fmt.Print(".")
+				}
+			}
+			fmt.Println()
+		}
+		fmt.Println()
+		os.Exit(0)
+	}
+}
