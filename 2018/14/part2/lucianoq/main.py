9ca617d9d3e141a8fca33086a88fc23d80581966
Luciano Quercia <luciano.quercia@gmail.com>
2018-12-21 13:19:30 +0100

    solution and bugfix for 2018.14/2

--- 2018/14/part2/lucianoq/main.py
+++ 2018/14/part2/lucianoq/main.py
@@ -1,29 +1,28 @@
 #!/usr/local/bin/python3
 
 
-def first_appears(input):
-    elf1 = 0
-    elf2 = 1
-    sb = "37"
+def first_appears(sequence):
+    elf_1 = 0
+    elf_2 = 1
+    scoreboard = "37"
+    input_len = len(sequence)
 
-    l = len(input)
     while True:
-        sum = int(sb[elf1]) + int(sb[elf2])
+        recipe1 = int(scoreboard[elf_1])
+        recipe2 = int(scoreboard[elf_2])
+        recipe_sum = recipe1 + recipe2
+        if recipe_sum > 9:
+            scoreboard += "1"
+            if len(scoreboard) >= input_len and scoreboard[-input_len:] == sequence:
+                return len(scoreboard) - input_len
+        scoreboard += str(recipe_sum % 10)
+        if len(scoreboard) >= input_len and scoreboard[-input_len:] == sequence:
+            return len(scoreboard) - input_len
 
-        if sum > 9:
-            sb += "1" + str(sum % 10)
-        else:
-            sb += str(sum)
-
-        print(sb[:2] == "37")
-        elf1 = (elf1 + 1 + int(sb[elf1])) % len(sb)
-        elf2 = (elf2 + 1 + int(sb[elf2])) % len(sb)
-        # dump(sb, elf1, elf2)
-        if len(sb) >= l and sb[-l:] == input:
-            # dump(sb, elf1, elf2)
-            return len(sb) - l
+        elf_1 = (elf_1 + 1 + recipe1) % len(scoreboard)
+        elf_2 = (elf_2 + 1 + recipe2) % len(scoreboard)
 
 
 if __name__ == '__main__':
-    input = "286051"
-    print(first_appears(input))
+    assignment = "286051"
+    print(first_appears(assignment))
