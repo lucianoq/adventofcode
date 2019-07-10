b72fcb15ca825e12de7fce7a6c1ea0e4a9eaf450
Luciano Quercia <luciano.quercia@gmail.com>
2019-07-10 20:15:40 +0100

    Solution for 2015, day 5, parts 1-2

---
+++ 2015/5/part1/lucianoq/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	i := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		if CountVowels(line) >= 3 && CountainsCoupleEqualChars(line) && NotContainsBadCouples(line) {
+			i++
+		}
+	}
+	fmt.Println(i)
+}
+
+func CountVowels(s string) (i int) {
+	for _, value := range s {
+		switch value {
+		case 'a', 'e', 'i', 'o', 'u':
+			i++
+		}
+	}
+	return
+}
+
+func CountainsCoupleEqualChars(s string) bool {
+	for i := 0; i < len(s)-1; i++ {
+		if s[i] == s[i+1] {
+			return true
+		}
+	}
+	return false
+}
+
+func NotContainsBadCouples(s string) bool {
+	for i := 0; i < len(s)-1; i++ {
+		switch s[i : i+2] {
+		case "ab", "cd", "pq", "xy":
+			return false
+		}
+	}
+	return true
+}
---
+++ 2015/5/part2/lucianoq/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+//qjhvhtzxzqqjkmpb is nice because is has a pair that appears twice (qj) and a
+// letter that repeats with exactly one letter between them (zxz).
+//xxyxx is nice because it has a pair that appears twice and a letter that
+// repeats with one between, even though the letters used by each rule overlap.
+//uurcxstgmygtbstg is naughty because it has a pair (tg) but no repeat with a
+// single letter between them.
+//ieodomkazucvgmuy is naughty because it has a repeating letter with one
+// between (odo), but no pair that appears twice.
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	i := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) > 3 {
+			if A(line) && B(line) {
+				i++
+			}
+		}
+
+	}
+	fmt.Println(i)
+}
+
+// It contains a pair of any two letters that appears at least twice in the
+// string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like
+// aaa (aa, but it overlaps).
+func A(s string) bool {
+	// -1 because I'm considering pairs
+	// -2 because first index stops when there is room for another pair with j.
+	for i := 0; i < len(s)-1-2; i++ {
+		for j := i + 2; j < len(s)-1; j++ {
+			if s[i:i+2] == s[j:j+2] {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// It contains at least one letter which repeats with exactly one letter between
+// them, like xyx, abcdefeghi (efe), or even aaa.
+func B(s string) bool {
+	for i := 0; i < len(s)-2; i++ {
+		if s[i] == s[i+2] {
+			return true
+		}
+	}
+	return false
+}
+
