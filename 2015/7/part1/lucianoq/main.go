8b9c3fbdb380f8611f974a9de118ab2d8f851126
Luciano Quercia <luciano.quercia@gmail.com>
2019-07-13 10:45:27 +0100

    solution for 2015.7, part 1/2

---
+++ 2015/7/part1/lucianoq/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+var mem map[string]uint16
+
+func main() {
+	content, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lines := strings.Split(string(content), "\n")
+	ops := make(map[string]bool)
+	for _, l := range lines {
+		if l != "" {
+			ops[l] = false
+		}
+	}
+
+	mem = make(map[string]uint16)
+
+	for {
+
+		if _, ok := mem["a"]; ok {
+			fmt.Println(mem["a"])
+			return
+		}
+
+		for line, ok := range ops {
+			if !ok {
+				success := exec(line)
+				ops[line] = success
+			}
+		}
+	}
+}
+
+func exec(line string) bool {
+	op, args := extract(line)
+
+	switch op {
+	case ":=":
+		return assignment(args[0], args[1])
+
+	case "NOT":
+		return not(args[0], args[1])
+
+	case "AND":
+		return and(args[0], args[1], args[2])
+
+	case "OR":
+		return or(args[0], args[1], args[2])
+
+	case "LSHIFT":
+		return lshift(args[0], args[1], args[2])
+
+	case "RSHIFT":
+
+		return rshift(args[0], args[1], args[2])
+
+	}
+	return false
+}
---
+++ 2015/7/part1/lucianoq/parse.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+func extract(s string) (string, []string) {
+	var op, res, a1, a2 string
+
+	var n int
+	var err error
+
+	n, err = fmt.Sscanf(s, "%s -> %s", &a1, &res)
+	if err == nil && n == 2 {
+		return ":=", []string{a1, res}
+	}
+
+	n, err = fmt.Sscanf(s, "NOT %s -> %s", &a1, &res)
+	if err == nil && n == 2 {
+		return "NOT", []string{a1, res}
+	}
+
+	n, err = fmt.Sscanf(s, "%s %s %s -> %s", &a1, &op, &a2, &res)
+	if err == nil && n == 4 {
+		return op, []string{a1, a2, res}
+	}
+
+	log.Fatalf("error with %s", s)
+
+	return "", nil
+}
+
+func isNum(s string) bool {
+	_, err := strconv.ParseUint(s, 10, 16)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func available(s string) bool {
+	_, ok := mem[s]
+	return ok
+}
