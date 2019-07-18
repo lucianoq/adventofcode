2a6ad266045f04b3ba8cd3e3f8b0c0d5c5be89c3
Luciano Quercia <luciano.quercia@gmail.com>
2019-07-18 21:55:40 +0100

    solution for 2015.16

---
+++ 2015/16/part1/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var MFCSAM = map[string]int{
+	"children":    3,
+	"cats":        7,
+	"samoyeds":    2,
+	"pomeranians": 3,
+	"akitas":      0,
+	"vizslas":     0,
+	"goldfish":    5,
+	"trees":       3,
+	"cars":        2,
+	"perfumes":    1,
+}
+
+func main() {
+	db := parse()
+
+	for i := 1; i < 501; i++ {
+		if compatible(db[i]) {
+			fmt.Println(i)
+		}
+	}
+}
+
+func compatible(test map[string]int) bool {
+	for k, v := range test {
+		if v != MFCSAM[k] {
+			return false
+		}
+	}
+	return true
+}
+
+func parse() [501]map[string]int {
+	db := [501]map[string]int{}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+
+		r, err := regexp.Compile("^Sue (\\d+): (.*)$")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		arr := r.FindStringSubmatch(line)
+		id, err := strconv.Atoi(arr[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		list := arr[2]
+
+		db[id] = make(map[string]int)
+
+		kvs := strings.Split(list, ", ")
+		for _, kv := range kvs {
+			xs := strings.Split(kv, ": ")
+			n, err := strconv.Atoi(xs[1])
+			if err != nil {
+				log.Fatal(err)
+			}
+			db[id][xs[0]] = n
+		}
+	}
+
+	return db
+}
