7f93e5252cc8e44f28b07974808e0f7061972898
Luciano Quercia <luciano.quercia@gmail.com>
2019-09-17 22:54:39 +0100

    go fmt

--- 2017/9/main1_test.go
+++ 2017/9/main1_test.go
@@ -15,44 +15,44 @@ func TestStream_ExtractGroup(t *testing.T) {
 		want int
 	}{
 		{
-			name:  "{}",
-			buf:   []byte("{}"),
-			want:  1,
+			name: "{}",
+			buf:  []byte("{}"),
+			want: 1,
 		},
 		{
-			name:  "{{{}}}",
-			buf:   []byte("{{{}}}"),
-			want:  6,
+			name: "{{{}}}",
+			buf:  []byte("{{{}}}"),
+			want: 6,
 		},
 		{
-			name:  "{{},{}}",
-			buf:   []byte("{{},{}}"),
-			want:  5,
+			name: "{{},{}}",
+			buf:  []byte("{{},{}}"),
+			want: 5,
 		},
 		{
-			name:  "{{{},{},{{}}}}",
-			buf:   []byte("{{{},{},{{}}}}"),
-			want:  16,
+			name: "{{{},{},{{}}}}",
+			buf:  []byte("{{{},{},{{}}}}"),
+			want: 16,
 		},
 		{
-			name:  "{<a>,<a>,<a>,<a>}",
-			buf:   []byte("{<a>,<a>,<a>,<a>}"),
-			want:  1,
+			name: "{<a>,<a>,<a>,<a>}",
+			buf:  []byte("{<a>,<a>,<a>,<a>}"),
+			want: 1,
 		},
 		{
-			name:  "{{<ab>},{<ab>},{<ab>},{<ab>}}",
-			buf:   []byte("{{<ab>},{<ab>},{<ab>},{<ab>}}"),
-			want:  9,
+			name: "{{<ab>},{<ab>},{<ab>},{<ab>}}",
+			buf:  []byte("{{<ab>},{<ab>},{<ab>},{<ab>}}"),
+			want: 9,
 		},
 		{
-			name:  "{{<!!>},{<!!>},{<!!>},{<!!>}}",
-			buf:   []byte("{{<!!>},{<!!>},{<!!>},{<!!>}}"),
-			want:  9,
+			name: "{{<!!>},{<!!>},{<!!>},{<!!>}}",
+			buf:  []byte("{{<!!>},{<!!>},{<!!>},{<!!>}}"),
+			want: 9,
 		},
 		{
-			name:  "{{<a!>},{<a!>},{<a!>},{<ab>}}",
-			buf:   []byte("{{<a!>},{<a!>},{<a!>},{<ab>}}"),
-			want:  3,
+			name: "{{<a!>},{<a!>},{<a!>},{<ab>}}",
+			buf:  []byte("{{<a!>},{<a!>},{<a!>},{<ab>}}"),
+			want: 3,
 		},
 	}
 	for _, tt := range tests {
