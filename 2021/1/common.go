67aac090ec6e71a9fc1b9725e4869f73d7ce89f8
Luciano Quercia <luciano.quercia@gmail.com>
2021-12-04 10:19:47 +0000

    missing common.go file in 2021.1

--- 2021/1/common.go
+++ 2021/1/common.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+)
+
+func parse() []int {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	list := []int{}
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		num, err := strconv.Atoi(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		list = append(list, num)
+	}
+	return list
+}
