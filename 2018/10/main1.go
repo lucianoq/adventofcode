fdafe18c189bb30ee8d2291b89876a2ab257d541
Luciano Quercia <luciano.quercia@gmail.com>
2021-12-14 19:14:59 +0000

    Adding string recognition to 2018/10

--- 2018/10/main1.go
+++ 2018/10/main1.go
@@ -1,52 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
 	"math"
 	"os"
-	"strings"
 )
-
-type Star struct {
-	X    int
-	Y    int
-	VelX int
-	VelY int
-}
-
-func (s *Star) Current(time int) (x, y int) {
-	return s.X + time*s.VelX, s.Y + time*s.VelY
-}
-
-var stars []*Star
-
-func main() {
-	stars = make([]*Star, 0)
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		if line != "" {
-			line = strings.Replace(line, " ", "", -1)
-			var x, y, velX, velY int
-			n, err := fmt.Sscanf(line, "position=<%d,%d>velocity=<%d,%d>", &x, &y, &velX, &velY)
-			if err != nil || n != 4 {
-				log.Fatal(err)
-			}
-			stars = append(stars, &Star{x, y, velX, velY})
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	for i := 0; ; i++ {
-		Sky(i)
-		//time.Sleep(200 * time.Millisecond)
-	}
-
-}
 
 func Sky(time int) {
 	interpolatedStars := make([]*Star, 0)
@@ -91,17 +49,7 @@ func Sky(time int) {
 			sky[s.X][s.Y] = true
 		}
 
-		for j := 0; j < maxY; j++ {
-			for i := 0; i < maxX; i++ {
-				if sky[i][j] {
-					fmt.Print("█")
-				} else {
-					fmt.Print(" ")
-				}
-			}
-			fmt.Println()
-		}
-		fmt.Println()
+		fmt.Println(toString(sky, maxX, maxY, 6, 2))
 		os.Exit(0)
 	}
 }
--- 2018/10/main2.go
+++ 2018/10/main2.go
@@ -1,51 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
 	"math"
 	"os"
-	"strings"
 )
-
-type Star struct {
-	X    int
-	Y    int
-	VelX int
-	VelY int
-}
-
-func (s *Star) Current(time int) (x, y int) {
-	return s.X + time*s.VelX, s.Y + time*s.VelY
-}
-
-var stars []*Star
-
-func main() {
-	stars = make([]*Star, 0)
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		if line != "" {
-			line = strings.Replace(line, " ", "", -1)
-			var x, y, velX, velY int
-			n, err := fmt.Sscanf(line, "position=<%d,%d>velocity=<%d,%d>", &x, &y, &velX, &velY)
-			if err != nil || n != 4 {
-				log.Fatal(err)
-			}
-			stars = append(stars, &Star{x, y, velX, velY})
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	for i := 0; ; i++ {
-		Sky(i)
-	}
-
-}
 
 func Sky(time int) {
 	interpolatedStars := make([]*Star, 0)
