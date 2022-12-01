7d33606c0908f26bccd7fc70cb58a8c9ff46ff12
Luciano Quercia <luciano.quercia@gmail.com>
2022-12-01 11:54:48 +0000

    Improved scaffolding and scrape

--- scrape.sh
+++ scrape.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/env bash
 
-COOKIE_TOKEN="$1"
-YEAR="$2"
-DAY="$3"
+YEAR="$1"
+DAY="$2"
 
 #create dir
 mkdir -p "$YEAR/$DAY"
+cd "$YEAR/$DAY" || exit
+
+touch output1 output2
+
+GO_TEMPLATE="package main\n\nfunc main() {\n    \n}\n"
+echo -en "$GO_TEMPLATE" | tee main1.go main2.go >/dev/null
+
+
+cat >Makefile <<EOF
+1:
+	go run main1.go <input
+
+2:
+	go run main2.go <input
+EOF
 
 # download input files
-http "https://adventofcode.com/$YEAR/day/$DAY/input" "Cookie:session=$COOKIE_TOKEN;" >"$YEAR/$DAY/input"
+http "https://adventofcode.com/$YEAR/day/$DAY/input" "Cookie:session=$AOC_SESSION;" >input
 
 # download assignment
-http "https://adventofcode.com/$YEAR/day/$DAY" "Cookie:session=$COOKIE_TOKEN;" | pup 'article.day-desc' >"$YEAR/$DAY/tmp.html"
-lynx -dump "$YEAR/$DAY/tmp.html" -width 80 >"$YEAR/$DAY/assignment"
-rm -f "$YEAR/$DAY/tmp.html"
+http "https://adventofcode.com/$YEAR/day/$DAY" "Cookie:session=$AOC_SESSION;" | pup 'article.day-desc' >tmp.html
+lynx -dump tmp.html -width 80 >assignment
+rm -f tmp.html
