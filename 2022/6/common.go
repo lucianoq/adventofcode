7b5b0c23699bcf8ab86b0cb47abeb0f3830e84ed
Luciano Quercia <luciano.quercia@gmail.com>
2022-12-06 18:19:51 +0000

    Using two cursor and a bitmap

--- 2022/6/common.go
+++ 2022/6/common.go
@@ -1,21 +1,38 @@
 package main
 
 func findMarker(line string, length int) int {
-	seen := map[byte]int{}
-	for i := 0; i < len(line); i++ {
-		l, ok := seen[line[i]]
-		if ok {
-			for k, v := range seen {
-				if v < l {
-					delete(seen, k)
+	var seen int32
+
+	var left, right = 0, 0
+
+	// move `right` forward
+	for ; ; right++ {
+
+		// if we find a duplicate character
+		if seen&(1<<(line[right]-'a')) != 0 {
+
+			// we move `left` forward, up to the next
+			// of the first occurrence of the duplicate
+			for ; ; left++ {
+
+				if line[left] == line[right] {
+					left++
+					break
 				}
+				seen &^= 1 << (line[left] - 'a')
 			}
+
+			// no need to add on `seen` (we didn't delete it)
+			// no need to check the length cause moving left
+			// shorten the string
+			continue
 		}
 
-		seen[line[i]] = i
-		if len(seen) == length {
-			return i + 1
+		seen |= 1 << (line[right] - 'a')
+
+		// if [left,right) is long enough, that's our goal
+		if right-left+1 == length {
+			return right + 1
 		}
 	}
-	return -1
 }
